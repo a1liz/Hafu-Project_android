e8626d5485980de548114dc267e79ed45f796347
a1liz <694426737@qq.com>
2017-08-07 23:45:47 +0800

    将userProfile部分添加到首页中

--- Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.xutils.view.annotation.ContentView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LayoutInflater from;
+    private View view;
     private TextView mTextMessage;
+
+    private LinearLayout linearLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +27,18 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    mTextMessage.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_home);
+                    view.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_dashboard:
+                    mTextMessage.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_dashboard);
+                    view.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setVisibility(View.GONE);
+                    getProfileView();
                     return true;
             }
             return false;
@@ -37,9 +51,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linearLayout = (LinearLayout) findViewById(R.id.topContainer);
+        from = LayoutInflater.from(MainActivity.this);
+        view = from.inflate(R.layout.main_profile,linearLayout);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void getProfileView() {
+        view.setVisibility(View.VISIBLE);
+
     }
 
 }
