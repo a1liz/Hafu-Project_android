8c7db85acd13ae231225efa920a7d63d68552339
a1liz <694426737@qq.com>
2017-08-07 19:44:14 +0800

    Initial commit

---
+++ Hafu/src/main/java/com/hafu/Hafu/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.hafu.Hafu;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
