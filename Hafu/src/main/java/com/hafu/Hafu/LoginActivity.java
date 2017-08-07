dc4a2be82f1809c6e87248fb33fe173ff17c0e40
a1liz <694426737@qq.com>
2017-08-07 23:24:48 +0800

    更改了背景颜色，完成了主页面的UserProfile部分

--- Hafu/src/main/java/com/hafu/Hafu/LoginActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/LoginActivity.java
@@ -1,15 +1,52 @@
 package com.hafu.Hafu;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+@ContentView(R.layout.activity_login)
 public class LoginActivity extends Activity {
+
+    @ViewInject(R.id.username)
+    private EditText username;
+    @ViewInject(R.id.password)
+    private EditText password;
+    private SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        x.view().inject(this);
+        sp = getSharedPreferences("msg",MODE_PRIVATE);
+        String user = sp.getString("username","");
+        String pwd = sp.getString("password", "");
+        username.setText(user);
+        password.setText(pwd);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("---->","onStop");
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("username",username.getText().toString());
+        editor.putString("password",password.getText().toString());
+        editor.commit();
+        Log.i("---->","存储成功!");
+    }
+
+    @Event(value = {R.id.login})
+    public void doEvent(View view) {
+
     }
 
 }
