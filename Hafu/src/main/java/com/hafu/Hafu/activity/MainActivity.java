bebae1b1c3d4105bc083a43176dea211dc24b816
a1liz <694426737@qq.com>
2017-08-08 19:09:13 +0800

    在userProfile页面增加了更改头像功能

--- Hafu/src/main/java/com/hafu/Hafu/activity/MainActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/activity/MainActivity.java
@@ -3,22 +3,36 @@ package com.hafu.Hafu.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hafu.Hafu.R;
+import com.hafu.Hafu.view.CircleImageView;
 
 import org.xutils.view.annotation.ViewInject;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,6 +47,24 @@ public class MainActivity extends Activity {
     private SharedPreferences sp;
     private LinearLayout linearLayout;
     private ListView order_list;
+
+    private CircleImageView ivHead;
+    private RelativeLayout layout_choose;
+    private RelativeLayout layout_photo;
+    private RelativeLayout layout_close;
+
+    private LinearLayout layout_all;
+    protected int mScreenWidth;
+
+    /**
+     * 定义三种状态
+     */
+    private static final int REQUESTCODE_PIC = 1;//相册
+    private static final int REQUESTCODE_CAM = 2;//相机
+    private static final int REQUESTCODE_CUT = 3;//图片裁剪
+
+    private Bitmap mBitmap;
+    private File mFile;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -93,6 +125,20 @@ public class MainActivity extends Activity {
         totalView.setVisibility(View.VISIBLE);
         if (sp.getString("isLogin","").equals("TRUE")) {
             totalView = from.inflate(R.layout.main_profile,linearLayout);
+
+            ivHead = (CircleImageView) totalView.findViewById(R.id.userIcon);
+            layout_all = (LinearLayout) totalView.findViewById(R.id.layout_all);
+
+            ivHead.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (view.getId()){
+                        case R.id.userIcon:
+                            showMyDialog();
+                            break;
+                    }
+                }
+            });
         } else if (sp.getString("isLogin","").equals("FALSE")) {
             totalView = from.inflate(R.layout.waiting_for_login,linearLayout);
         } else {
@@ -176,5 +222,186 @@ public class MainActivity extends Activity {
         startActivity(intent);
     }
 
+    // 头像选择部分
+    PopupWindow avatorPop;
+    private void showMyDialog() {
+        View view = LayoutInflater.from(this).inflate(R.layout.pop_show_dialog,
+                null);
+        layout_choose = (RelativeLayout) view.findViewById(R.id.layout_choose);
+        layout_photo = (RelativeLayout) view.findViewById(R.id.layout_photo);
+        layout_close = (RelativeLayout) view.findViewById(R.id.layout_close);
+
+        layout_choose.setBackgroundColor(getResources().getColor(
+                R.color.base_color_text_white));
+        layout_photo.setBackgroundDrawable(getResources().getDrawable(
+                R.drawable.pop_bg_press));
+        layout_close.setBackgroundColor(getResources().getColor(
+                R.color.base_color_text_white));
+
+
+        layout_photo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                layout_choose.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                layout_photo.setBackgroundDrawable(getResources().getDrawable(
+                        R.drawable.pop_bg_press));
+                layout_close.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+
+
+                openCamera();
+
+                // Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                //startActivityForResult(intent,);
+            }
+        });
+
+        layout_choose.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                layout_photo.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                layout_choose.setBackgroundDrawable(getResources().getDrawable(
+                        R.drawable.pop_bg_press));
+                layout_close.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                openPic();
+
+            }
+        });
+
+        layout_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout_photo.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                layout_close.setBackgroundDrawable(getResources().getDrawable(
+                        R.drawable.pop_bg_press));
+                layout_choose.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                avatorPop.dismiss();
+            }
+        });
+
+
+
+        DisplayMetrics metric = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metric);
+        mScreenWidth = metric.widthPixels;
+        avatorPop = new PopupWindow(view, mScreenWidth, 200);
+        avatorPop.setTouchInterceptor(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    avatorPop.dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        avatorPop.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+        avatorPop.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        avatorPop.setTouchable(true);
+        avatorPop.setFocusable(true);
+        avatorPop.setOutsideTouchable(true);
+        avatorPop.setBackgroundDrawable(new BitmapDrawable());
+        // 动画效果 从底部弹起
+        avatorPop.setAnimationStyle(R.style.Animations_GrowFromBottom);
+        avatorPop.showAtLocation(layout_all, Gravity.BOTTOM, 0, 0);
+    }
+
+    /**
+     * 打开相册
+     */
+    private void openPic() {
+        Intent picIntent = new Intent(Intent.ACTION_PICK,null);
+        picIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+        startActivityForResult(picIntent,REQUESTCODE_PIC);
+    }
+
+    /**
+     * 调用相机
+     */
+    private void openCamera() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if (!file.exists()){
+                file.mkdirs();
+            }
+            mFile = new File(file, System.currentTimeMillis() + ".jpg");
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mFile));
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+            startActivityForResult(intent,REQUESTCODE_CAM);
+        } else {
+            Toast.makeText(this, "请确认已经插入SD卡", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUESTCODE_CAM:
+                    startPhotoZoom(Uri.fromFile(mFile));
+                    break;
+                case REQUESTCODE_PIC:
+
+                    if (data == null || data.getData() == null){
+                        return;
+                    }
+                    startPhotoZoom(data.getData());
+
+                    break;
+                case REQUESTCODE_CUT:
+
+                    if (data!= null){
+                        setPicToView(data);
+                    }
+                    break;
+            }
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setPicToView(Intent data) {
+        Bundle bundle =  data.getExtras();
+        if (bundle != null){
+            //这里也可以做文件上传
+            mBitmap = bundle.getParcelable("data");
+            ivHead.setImageBitmap(mBitmap);
+        }
+    }
+
+    /**
+     * 打开系统图片裁剪功能
+     * @param uri
+     */
+    private void startPhotoZoom(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri,"image/*");
+        intent.putExtra("crop",true);
+        intent.putExtra("aspectX",1);
+        intent.putExtra("aspectY",1);
+        intent.putExtra("outputX",300);
+        intent.putExtra("outputY",300);
+        intent.putExtra("scale",true); //黑边
+        intent.putExtra("scaleUpIfNeeded",true); //黑边
+        intent.putExtra("return-data",true);
+        intent.putExtra("noFaceDetection",true);
+        startActivityForResult(intent,REQUESTCODE_CUT);
+
+    }
 
 }
