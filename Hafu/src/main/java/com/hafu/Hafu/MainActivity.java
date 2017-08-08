578968bfad44d5d925e9e42b817c3381be9f7f46
a1liz <694426737@qq.com>
2017-08-08 11:54:00 +0800

    完成了未登录时的Profile页面

--- Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hafu.Hafu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -21,10 +22,10 @@ import org.xutils.view.annotation.ViewInject;
 public class MainActivity extends Activity {
 
     private LayoutInflater from;
-    private View view;
+    private View totalView;
     private TextView mTextMessage;
-
-    private Button edit_address,about_us,remained_money,coupons,points;
+    private Drawable drawable;
+    private Button edit_address,about_us,remained_money,coupons,points,contact_us;
 
     private LinearLayout linearLayout;
 
@@ -37,12 +38,12 @@ public class MainActivity extends Activity {
                 case R.id.navigation_home:
                     mTextMessage.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_home);
-                    view.setVisibility(View.GONE);
+                    totalView.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setVisibility(View.VISIBLE);
                     mTextMessage.setText(R.string.title_dashboard);
-                    view.setVisibility(View.GONE);
+                    totalView.setVisibility(View.GONE);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setVisibility(View.GONE);
@@ -62,8 +63,8 @@ public class MainActivity extends Activity {
         //引入各分页布局文件
         linearLayout = (LinearLayout) findViewById(R.id.topContainer);
         from = LayoutInflater.from(MainActivity.this);
-        view = from.inflate(R.layout.main_profile,linearLayout);
-        view.setVisibility(View.GONE);
+        totalView = from.inflate(R.layout.main_profile,linearLayout);
+        totalView.setVisibility(View.GONE);
 
         mTextMessage = (TextView) findViewById(R.id.message);
 
@@ -71,15 +72,42 @@ public class MainActivity extends Activity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        setLoggedInProfileView();
+
+    }
+
+    /**
+     *  获取个人详情页面
+     */
+    private void getProfileView() {
+        totalView.setVisibility(View.VISIBLE);
+
+    }
+
+    public void logout(View view) {
+        linearLayout.removeAllViews();
+        totalView = from.inflate(R.layout.waiting_for_login,linearLayout);
+        setLoggedOutProfileView();
 
 
-        edit_address = view.findViewById(R.id.edit_address);
-        about_us = view.findViewById(R.id.about_us);
-        remained_money = view.findViewById(R.id.remained_money);
-        coupons = view.findViewById(R.id.coupons);
-        points = view.findViewById(R.id.points);
+    }
 
-        Drawable drawable;
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     *  设置已登录状态下的Profile页面
+     *
+     */
+    private void setLoggedInProfileView() {
+        edit_address = totalView.findViewById(R.id.edit_address);
+        about_us = totalView.findViewById(R.id.about_us);
+        remained_money = totalView.findViewById(R.id.remained_money);
+        coupons = totalView.findViewById(R.id.coupons);
+        points = totalView.findViewById(R.id.points);
+
         // 为remianed_money按钮设置下侧图标
         drawable = getResources().getDrawable(R.drawable.remain_money,null);
         drawable.setBounds(0,0,70,50);
@@ -95,12 +123,46 @@ public class MainActivity extends Activity {
         drawable.setBounds(0,0,70,50);
         points.setCompoundDrawables(null,null,null,drawable);
 
+        // 为about_us按钮设置左侧图标
+        drawable = getResources().getDrawable(R.drawable.info,null);
+        drawable.setBounds(0,0,40,40);
+        about_us.setCompoundDrawables(drawable,null,null,null);
+        about_us.setCompoundDrawablePadding(20);
+        about_us.setPadding(20,0,0,0);
+
         // 为edit_addresss按钮设置左侧图标
         drawable = getResources().getDrawable(R.drawable.pin,null);
         drawable.setBounds(0,0,40,40);
         edit_address.setCompoundDrawables(drawable,null,null,null);
         edit_address.setCompoundDrawablePadding(20);
         edit_address.setPadding(20,0,0,0);
+    }
+
+    /**
+     *  设置未登录状态下的Profile页面
+     *
+     */
+    private void setLoggedOutProfileView() {
+        about_us = totalView.findViewById(R.id.about_us);
+        remained_money = totalView.findViewById(R.id.remained_money);
+        coupons = totalView.findViewById(R.id.coupons);
+        points = totalView.findViewById(R.id.points);
+        contact_us = totalView.findViewById(R.id.contact_us);
+
+        // 为remianed_money按钮设置下侧图标
+        drawable = getResources().getDrawable(R.drawable.remain_money,null);
+        drawable.setBounds(0,0,70,50);
+        remained_money.setCompoundDrawables(null,null,null,drawable);
+
+        // 为coupons按钮设置下侧图标
+        drawable = getResources().getDrawable(R.drawable.coupons,null);
+        drawable.setBounds(0,0,70,50);
+        coupons.setCompoundDrawables(null,null,null,drawable);
+
+        // 为points按钮设置下侧图标
+        drawable = getResources().getDrawable(R.drawable.points,null);
+        drawable.setBounds(0,0,70,50);
+        points.setCompoundDrawables(null,null,null,drawable);
 
         // 为about_us按钮设置左侧图标
         drawable = getResources().getDrawable(R.drawable.info,null);
@@ -109,16 +171,11 @@ public class MainActivity extends Activity {
         about_us.setCompoundDrawablePadding(20);
         about_us.setPadding(20,0,0,0);
 
-
-
+        // 为contact_us按钮设置左侧图标
+        drawable = getResources().getDrawable(R.drawable.contact,null);
+        drawable.setBounds(0,0,40,40);
+        contact_us.setCompoundDrawables(drawable,null,null,null);
+        contact_us.setCompoundDrawablePadding(20);
+        contact_us.setPadding(20,0,0,0);
     }
-
-    /**
-     *  获取个人详情页面
-     */
-    private void getProfileView() {
-        view.setVisibility(View.VISIBLE);
-
-    }
-
 }
