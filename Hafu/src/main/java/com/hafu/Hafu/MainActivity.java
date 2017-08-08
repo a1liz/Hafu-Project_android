cf3966fa19f2ad7296206c9c6f09c1da3f39ea09
a1liz <694426737@qq.com>
2017-08-08 13:19:50 +0800

    更改了Profile页面的生成方式

--- Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/MainActivity.java
@@ -24,8 +24,7 @@ public class MainActivity extends Activity {
     private LayoutInflater from;
     private View totalView;
     private TextView mTextMessage;
-    private Drawable drawable;
-    private Button edit_address,about_us,remained_money,coupons,points,contact_us;
+    
 
     private LinearLayout linearLayout;
 
@@ -71,9 +70,6 @@ public class MainActivity extends Activity {
         // 引入底部导航栏
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        setLoggedInProfileView();
-
     }
 
     /**
@@ -87,95 +83,10 @@ public class MainActivity extends Activity {
     public void logout(View view) {
         linearLayout.removeAllViews();
         totalView = from.inflate(R.layout.waiting_for_login,linearLayout);
-        setLoggedOutProfileView();
-
-
     }
 
     public void login(View view) {
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
-    }
-
-    /**
-     *  设置已登录状态下的Profile页面
-     *
-     */
-    private void setLoggedInProfileView() {
-        edit_address = totalView.findViewById(R.id.edit_address);
-        about_us = totalView.findViewById(R.id.about_us);
-        remained_money = totalView.findViewById(R.id.remained_money);
-        coupons = totalView.findViewById(R.id.coupons);
-        points = totalView.findViewById(R.id.points);
-
-        // 为remianed_money按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.remain_money,null);
-        drawable.setBounds(0,0,70,50);
-        remained_money.setCompoundDrawables(null,null,null,drawable);
-
-        // 为coupons按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.coupons,null);
-        drawable.setBounds(0,0,70,50);
-        coupons.setCompoundDrawables(null,null,null,drawable);
-
-        // 为points按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.points,null);
-        drawable.setBounds(0,0,70,50);
-        points.setCompoundDrawables(null,null,null,drawable);
-
-        // 为about_us按钮设置左侧图标
-        drawable = getResources().getDrawable(R.drawable.info,null);
-        drawable.setBounds(0,0,40,40);
-        about_us.setCompoundDrawables(drawable,null,null,null);
-        about_us.setCompoundDrawablePadding(20);
-        about_us.setPadding(20,0,0,0);
-
-        // 为edit_addresss按钮设置左侧图标
-        drawable = getResources().getDrawable(R.drawable.pin,null);
-        drawable.setBounds(0,0,40,40);
-        edit_address.setCompoundDrawables(drawable,null,null,null);
-        edit_address.setCompoundDrawablePadding(20);
-        edit_address.setPadding(20,0,0,0);
-    }
-
-    /**
-     *  设置未登录状态下的Profile页面
-     *
-     */
-    private void setLoggedOutProfileView() {
-        about_us = totalView.findViewById(R.id.about_us);
-        remained_money = totalView.findViewById(R.id.remained_money);
-        coupons = totalView.findViewById(R.id.coupons);
-        points = totalView.findViewById(R.id.points);
-        contact_us = totalView.findViewById(R.id.contact_us);
-
-        // 为remianed_money按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.remain_money,null);
-        drawable.setBounds(0,0,70,50);
-        remained_money.setCompoundDrawables(null,null,null,drawable);
-
-        // 为coupons按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.coupons,null);
-        drawable.setBounds(0,0,70,50);
-        coupons.setCompoundDrawables(null,null,null,drawable);
-
-        // 为points按钮设置下侧图标
-        drawable = getResources().getDrawable(R.drawable.points,null);
-        drawable.setBounds(0,0,70,50);
-        points.setCompoundDrawables(null,null,null,drawable);
-
-        // 为about_us按钮设置左侧图标
-        drawable = getResources().getDrawable(R.drawable.info,null);
-        drawable.setBounds(0,0,40,40);
-        about_us.setCompoundDrawables(drawable,null,null,null);
-        about_us.setCompoundDrawablePadding(20);
-        about_us.setPadding(20,0,0,0);
-
-        // 为contact_us按钮设置左侧图标
-        drawable = getResources().getDrawable(R.drawable.contact,null);
-        drawable.setBounds(0,0,40,40);
-        contact_us.setCompoundDrawables(drawable,null,null,null);
-        contact_us.setCompoundDrawablePadding(20);
-        contact_us.setPadding(20,0,0,0);
     }
 }
