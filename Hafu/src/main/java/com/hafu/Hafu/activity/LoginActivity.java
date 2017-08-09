f2a0c188b7ba98662ae79fad549581763e428a11
a1liz <694426737@qq.com>
2017-08-10 00:22:50 +0800

    完成了登录部分与服务器端的连接

--- Hafu/src/main/java/com/hafu/Hafu/activity/LoginActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/activity/LoginActivity.java
@@ -6,9 +6,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 import com.hafu.Hafu.R;
@@ -19,6 +22,15 @@ import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @ContentView(R.layout.activity_login)
 public class LoginActivity extends Activity {
@@ -31,6 +43,37 @@ public class LoginActivity extends Activity {
     @ViewInject(R.id.password)
     private EditText password;
     private SharedPreferences sp;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 1) {
+                String result = (String) msg.obj;
+                Log.i("结果：",result);
+                SharedPreferences.Editor editor = sp.edit();
+                if (result.equals("FALSE")) {
+                    editor.putString("isLogin","FALSE");
+                } else {
+                    editor.putString("uid", result);
+                    editor.putString("isLogin", "TRUE");
+                }
+                editor.commit();
+
+                String isLogin = sp.getString("isLogin","");
+                Log.i("isLogin = ",isLogin);
+                if (isLogin.equals("TRUE")) {
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                } else if (isLogin.equals("FALSE")) {
+                    Toast.makeText(LoginActivity.this,"用户名或密码错误",Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.i("ERROR ====>","缓存中isLogin字段错误");
+                }
+            }
+        }
+    };
+    OkHttpClient okHttpClient = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,13 +115,20 @@ public class LoginActivity extends Activity {
 
     @Event(value = {R.id.login})
     public void doEvent(View view) {
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString("username",username.getText().toString());
-        editor.putString("isLogin","TRUE");
-        editor.commit();
+//        SharedPreferences.Editor editor = sp.edit();
+//        editor.putString("username",username.getText().toString());
+//        editor.putString("isLogin","TRUE");
+//        editor.commit();
 
-        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-        startActivity(intent);
+        FormBody.Builder builder1 = new FormBody.Builder();
+        FormBody formBody = builder1.add("id",username.getText().toString())
+                .add("pwd",password.getText().toString()).build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request1 = builder.url("http://10.187.133.227:8080/hafu_project/login_android")
+                .post(formBody)
+                .build();
+        exec(request1);
     }
 
     @Event(value = {R.id.back})
@@ -87,5 +137,23 @@ public class LoginActivity extends Activity {
         startActivity(intent);
     }
 
+    private void exec(Request request) {
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("异常：","-->"+e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i("成功：","--->");
+                String s = response.body().string();
+                Message message = new Message();
+                message.what = 1;
+                message.obj = s;
+                handler.sendMessage(message);
+            }
+        });
+    }
 
 }
