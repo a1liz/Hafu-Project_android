0d568b8df971e80dba734152dd38ba451877f2b8
chamifuoer <342516647@qq.com>
2017-08-10 05:20:09 +0800

    增加了注册页面的验证码功能  其中verificationCode  类就是 验证码的随机生成 之后再注册页面实现了显示

--- Hafu/src/main/java/com/hafu/Hafu/activity/RegisterActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/activity/RegisterActivity.java
@@ -1,13 +1,19 @@
 package com.hafu.Hafu.activity;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.hafu.Hafu.R;
+import com.hafu.Hafu.view.VerificationCode;
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerTextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    private ImageView showCode;
+    private String realCode;
     ShimmerTextView tv;
     Shimmer shimmer;
 
@@ -19,6 +25,16 @@ public class RegisterActivity extends AppCompatActivity {
 
         shimmer = new Shimmer();    //启动字体闪烁
         shimmer.start(tv);
+
+
+
+
+        showCode = (ImageView) findViewById(R.id.iv_showCode);
+        //将验证码用图片的形式显示出来
+        showCode.setImageBitmap(VerificationCode.getInstance().createBitmap());
+        realCode = VerificationCode.getInstance().getCode().toLowerCase();
+
+
     }
 
 }
