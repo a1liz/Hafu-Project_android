ff34225ad3df64181ca1d28edb00d77057786e35
a1liz <694426737@qq.com>
2017-08-09 16:13:12 +0800

    修复了登陆页面关闭错误的bug

--- Hafu/src/main/java/com/hafu/Hafu/activity/LoginActivity.java
+++ Hafu/src/main/java/com/hafu/Hafu/activity/LoginActivity.java
@@ -81,5 +81,11 @@ public class LoginActivity extends Activity {
         startActivity(intent);
     }
 
+    @Event(value = {R.id.back})
+    public void exitToHome(View view) {
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
+    }
+
 
 }
